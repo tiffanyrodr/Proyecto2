3deffe54a3535a256804e7d0a57beb3a265decfb
tiffanyrodr <tiff18448@gmail.com>
2026-06-12 13:00:19 -0600

    Inicializar base de datos SQLite en layout principal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,37 @@
+import { expoDb } from "@/db/client";
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
 
 export default function RootLayout() {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    try {
+      expoDb.execSync(`
+        CREATE TABLE IF NOT EXISTS partidos (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          equipo_local TEXT NOT NULL,
+          equipo_visitante TEXT NOT NULL,
+          goles_local INTEGER NOT NULL DEFAULT 0,
+          goles_visitante INTEGER NOT NULL DEFAULT 0,
+          fecha TEXT NOT NULL,
+          grupo TEXT NOT NULL
+        );
+      `);
+      setInit(true);
+    } catch (error) {
+      console.error("Error al iniciar", error);
+    }
+  }, []);
+
+  if (!init) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
+
   return <Stack />;
 }
--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
 
-const expoDb = openDatabaseSync("partidos.db", { enableChangeListener: true });
+export const expoDb = openDatabaseSync("partidos.db", { enableChangeListener: true });
 
 export const db = drizzle(expoDb);
